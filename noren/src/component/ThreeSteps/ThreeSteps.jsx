b87ba224a944ab80d8fdd5df6ede465cb78858fb
FengDenny <fengdennyy@gmail.com>
2022-08-24 18:12:54 -0700

    Create ThreeSteps.jsx

---
+++ noren/src/component/ThreeSteps/ThreeSteps.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import ThreeStepStyles from './ThreeSteps.module.css';
+import globalStyles from '../../global.module.css';
+import { steps } from '../../data/threeSteps';
+import Section3 from '../../img/section3.png';
+export default function ThreeSteps() {
+  return (
+    <div className={ThreeStepStyles.container}>
+      <div
+        className={`${globalStyles.container} ${globalStyles.dFlex} ${globalStyles.justifySpaceAround}`}
+      >
+        <div className={ThreeStepStyles.sectionLeft}>
+          <img src={Section3} alt="Steps" />
+        </div>
+        <div className={ThreeStepStyles.sectionRight}>
+          <h2>We take care of your business</h2>
+          <p>
+            Power your business to new heights with our award-winning digital
+            marketing services and technology platform.
+          </p>
+
+          {steps.map((data) => {
+            const { id, title } = data;
+            return (
+              <div className={ThreeStepStyles.circleGray} key={id}>
+                {id === 1 && (
+                  <>
+                    <div className={ThreeStepStyles.circleOne}>
+                      <h2>{id}</h2>
+                    </div>
+                    <h3>{title}</h3>
+                  </>
+                )}
+                {id === 2 && (
+                  <>
+                    <div className={ThreeStepStyles.circleTwo}>
+                      <h2>{id}</h2>
+                    </div>
+                    <h3>{title}</h3>
+                  </>
+                )}
+                {id === 3 && (
+                  <>
+                    <div className={ThreeStepStyles.circleThree}>
+                      <h2>{id}</h2>
+                    </div>
+                    <h3>{title}</h3>
+                  </>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
