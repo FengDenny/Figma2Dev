aa603cb9f764cb37fd153e8c0ca20381511211fc
FengDenny <fengdennyy@gmail.com>
2022-08-25 13:01:31 -0700

    created Blog section / data

---
+++ noren/src/component/Blog/Blog.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import globalStyles from '../../global.module.css';
+import BlogStyles from './blog.module.css';
+import Card from './BlogCard';
+import { blogData } from '../../data/blog';
+
+export default function Blog() {
+  return (
+    <section className={BlogStyles.container}>
+      <div className={`${globalStyles.container}`}>
+        <div className={BlogStyles.header}>
+          <h2>From Blog</h2>
+        </div>
+        <div className={globalStyles.grid}>
+          {blogData.map((data) => {
+            return <Card blogData={data} key={data.id} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ noren/src/component/Blog/BlogCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import BlogStyles from './blog.module.css';
+import { FaCalendarAlt, FaHourglassEnd } from 'react-icons/fa';
+export default function BlogCard({
+  blogData: { title, description, date, duration, image },
+}) {
+  return (
+    <article className={BlogStyles.card}>
+      <img src={require(`../../img/${image}`)} alt={title} />
+      <div className={BlogStyles.blogInfo}>
+        <div className={`${BlogStyles.icon} ${BlogStyles.date}`}>
+          <FaCalendarAlt />
+        </div>
+        <h4>{date}</h4>
+        <div className={`${BlogStyles.icon} ${BlogStyles.duration}`}>
+          <FaHourglassEnd />
+        </div>
+        <h4>{duration}</h4>
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </article>
+  );
+}
