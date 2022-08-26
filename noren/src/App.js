c6acd33e2b99728f113b2859d78960a966d599b2
FengDenny <fengdennyy@gmail.com>
2022-08-25 17:51:59 -0700

    Created Footer/ styling

--- noren/src/App.js
+++ noren/src/App.js
@@ -1,11 +1,13 @@
-import Navbar from "./component/Navbar/Navbar";
-import Home from "./pages/home";
+import Navbar from './component/Navbar/Navbar';
+import Home from './pages/home';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
