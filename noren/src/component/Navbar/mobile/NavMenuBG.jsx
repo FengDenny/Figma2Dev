beec7e16be296b9b77eeb7d63dd8071404f4a156
FengDenny <fengdennyy@gmail.com>
2022-08-30 16:42:19 -0700

    file name changed

--- noren/src/component/Navbar/mobile/NavMenuBG.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MobileNavStyle from './mobileNav.module.css';
-
-export default function NavmenuBG(props) {
-  return (
-    <div className={MobileNavStyle.navMenuBackground} onClick={props.click} />
-  );
-}
