6e429f1f8606d22229c1877230740e6457edb544
FengDenny <fengdennyy@gmail.com>
2022-08-30 16:35:51 -0700

    Added Nav/ mobile nav, main pages, footer

--- Noren_v2/client/src/App.js
+++ Noren_v2/client/src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from "react";
+
+import Navbar from "./component/Navbar/desktop/Navbar";
+import NavMenuBG from "./component/Navbar/mobile/NavMenuBG";
+import MobileNav from "./component/Navbar/mobile/MobileMenu";
+import Home from "./pages/Home";
+import Footer from "./component/Footer/Footer";
 function App() {
+  let navMenuBG;
+  const [openMobile, setOpenMobile] = useState(false);
+
+  const btnToggleHandler = () => {
+    return setOpenMobile(!openMobile);
+  };
+
+  const closeNavMenu = () => {
+    return setOpenMobile(false);
+  };
+  if (openMobile) {
+    navMenuBG = <NavMenuBG click={closeNavMenu} />;
+  }
   return (
-    <div className='App'>
-      <h1>App</h1>
+    <div>
+      <Navbar btnToggleHandler={btnToggleHandler} />
+      <MobileNav show={openMobile} close={closeNavMenu} />
+      {navMenuBG}
+      <main>
+        <Home />
+      </main>
+      <Footer />
     </div>
   );
 }
