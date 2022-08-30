062ecf5a298db54a1e0477581a82adbc1e0eb5b0
FengDenny <fengdennyy@gmail.com>
2022-08-30 08:18:45 -0700

    Client's starting phase

---
+++ Noren_v2/client/src/data/blog.js
@@ -0,0 +1,29 @@
+export const blogData = [
+  {
+    id: 1,
+    title: 'Waiting list management',
+    description:
+      "The waitlist is an invaluable marketing tool when used appropriately. Here's how to capture those contacts.",
+    date: '23 Aug 2020',
+    duration: '12 min',
+    image: 'blog1.png',
+  },
+  {
+    id: 2,
+    title: 'E-commerce update',
+    description:
+      "The waitlist is an invaluable marketing tool when used appropriately. Here's how to capture those contacts.",
+    date: '24 Sep 2021',
+    duration: '10 min',
+    image: 'blog2.png',
+  },
+  {
+    id: 3,
+    title: 'Shopify’s Q2 Report',
+    description:
+      "The waitlist is an invaluable marketing tool when used appropriately. Here's how to capture those contacts.",
+    date: '13 Feb 2022',
+    duration: '15 min',
+    image: 'blog3.png',
+  },
+];
---
+++ Noren_v2/client/src/data/numbers.js
@@ -0,0 +1,17 @@
+export const numbersData = [
+  {
+    id: 1,
+    number: 300,
+    title: 'Projects',
+  },
+  {
+    id: 2,
+    number: 14000,
+    title: 'Users',
+  },
+  {
+    id: 3,
+    number: 140,
+    title: 'Employee',
+  },
+];
