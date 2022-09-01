4e234685550cce29e4a3a5502ddfee778bba4a3a
FengDenny <fengdennyy@gmail.com>
2022-09-01 13:58:39 -0700

    Get Started Modal popup

--- Noren/client/src/component/Hero/Hero.jsx
+++ Noren/client/src/component/Hero/Hero.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroStyles from "./Hero.module.css";
 import globalStyles from "../../global.module.css";
 
 import HeroImage from "../../img/heroRight.png";
+import Modal from "../Modal/Modal";
 export default function Hero() {
+  const [showModal, setShowModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className={HeroStyles.container}>
       <section
@@ -15,7 +26,21 @@ export default function Hero() {
             Power your business to new heights with our award-winning digital
             marketing services and technology platform.
           </p>
-          <button>Get Started</button>
+          <button onClick={() => setShowModal(!showModal)}>Get Started</button>
+          {showModal && (
+            <Modal
+              title='Sign up to get started'
+              labelOne='Email Address'
+              labelTwo='Password'
+              btnLabel='Sign Up'
+              email={email}
+              password={password}
+              closeModal={() => setShowModal(!showModal)}
+              setEmail={(e) => setEmail(e.target.value)}
+              setPassword={(e) => setPassword(e.target.value)}
+              handleSignUp={(e) => handleSignUp(e)}
+            />
+          )}
         </div>
         <div className={HeroStyles.heroImg}>
           <img src={HeroImage} alt='hero' />
