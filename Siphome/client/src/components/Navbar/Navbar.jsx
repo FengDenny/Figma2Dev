f11193758dfe79c422bd5eac4c7510287b1e1d67
FengDenny <fengdennyy@gmail.com>
2022-09-09 16:15:31 -0700

    created desktop nav/ css

---
+++ Siphome/client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import Logo from "../../img/logo.png";
+import MobileMenu from "./mobile/MobileMenu";
+import "./Navbar.scss";
+import { links } from "../../data/navbar";
+
+export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [data, setData] = useState(links);
+
+  return (
+    <>
+      <div className={"nav-container"}>
+        <nav className='container'>
+          <div className='d-flex-row justify-space-between'>
+            <img src={Logo} alt='Logo' />
+            <button
+              className='btn-nav-menu'
+              onClick={() => setOpenMenu(!openMenu)}
+            >
+              Menu
+            </button>
+            <ul className='desktop-nav'>
+              {data.map((data) => {
+                const { id, link, ahref } = data;
+                console.log(id, link, ahref);
+
+                return (
+                  <li className='desktop-nav-li' key={id}>
+                    <a href={ahref}>{link}</a>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className='desktop-nav-btn'>
+              <button className='desktop-btn-left'>Sign In</button>
+              <div className='vertical-line' />
+              <button className='desktop-btn-right'>Get Started</button>
+            </div>
+          </div>
+        </nav>
+      </div>
+      {openMenu && (
+        <div className={`nav-wrapper-${openMenu}`}>
+          <div className={`side-menu side-menu-${openMenu}`}>
+            <MobileMenu
+              close={() => setOpenMenu(!openMenu)}
+              data={data}
+              header='Siphome Menu'
+              leftBtn='Get Started'
+              // leftBtnClick ={console.log('clicked')}
+              rightBtn='Sign In'
+              rightBtnClick={console.log("clicked")}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- Siphome/client/src/components/Navbar/mobile/MobileMenu.jsx
+++ Siphome/client/src/components/Navbar/mobile/MobileMenu.jsx
@@ -5,12 +5,12 @@ export default function MobileMenu(props) {
   return (
     <>
       <div className='d-flex-row justify-space-between'>
-        <h2 className='menu-header'>Siphome Menu</h2>
+        <h2 className='menu-header'>{props.header}</h2>
         <button className='btn-close' onClick={props.close}>
           Close
         </button>
       </div>
-      <nav className='nav-mobile'>
+      <nav className='nav-mobile '>
         <ul className='nav-mobile-ul'>
           {props.data.map((data) => {
             const { id, link, ahref } = data;
@@ -24,6 +24,14 @@ export default function MobileMenu(props) {
           })}
         </ul>
       </nav>
+      <div className='btn-nav d-flex-row justify-space-between'>
+        <button className='btn-left' onClick={props.leftBtnClick}>
+          {props.leftBtn}
+        </button>
+        <button className='btn-right' onClick={props.rightBtnClick}>
+          {props.rightBtn}
+        </button>
+      </div>
     </>
   );
 }
