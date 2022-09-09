0780fb444eee03849719f5f8fd2f258bfa7ee59d
FengDenny <fengdennyy@gmail.com>
2022-09-08 19:34:36 -0700

    added mobile nav/ created global.scss

--- Siphome/client/src/App.js
+++ Siphome/client/src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+import MobileNav from "./components/Navbar/mobile/MobileNav";
+import MobileNavMenu from "./components/Navbar/mobile/MobileMenu";
+import MobileMenuBg from "./components/Navbar/mobile/MobileNavBG";
+import "./global.scss";
+
 function App() {
+  let navMenuBg;
+  const [openMenu, setOpenMenu] = useState(false);
+  const closeNavMenu = () => setOpenMenu(false);
+
+  if (openMenu) return (navMenuBg = <MobileMenuBg click={closeNavMenu} />);
   return (
-    <div className='App'>
-      <h2>App</h2>
+    <div>
+      <MobileNav />
+      {/* {navMenuBg} */}
     </div>
   );
 }
