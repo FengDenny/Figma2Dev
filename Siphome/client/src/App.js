439badef80a1b46ab1ea882c92b209ab2b04fc3d
FengDenny <fengdennyy@gmail.com>
2022-09-12 13:20:35 -0700

    removed home folder in component folder  to pages

--- Siphome/client/src/App.js
+++ Siphome/client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./pages/Home/Home";
 import "./global.scss";
 
 function App() {
