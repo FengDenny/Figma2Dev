2943764f43d105c394453a7c685bc9e78e4b4d99
FengDenny <fengdennyy@gmail.com>
2022-09-12 09:08:23 -0700

    removed global variables to its own folder and call it in each scss file

--- Siphome/client/src/components/Home/Home.jsx
+++ Siphome/client/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "../Hero/Hero";
-
+import Features from "../Features/Features";
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
---
+++ Siphome/client/src/data/features.js
@@ -0,0 +1,45 @@
+export const featuresLeft = [
+  {
+    id: 0,
+    title:
+      "Every day your home faces countless potential threats. Every day you face countless potential headaches. A small problem can become a major disaster. Early warning signs exist but often you can't hear or see them.",
+    image: "./section1.png",
+  },
+];
+
+export const featuresRight = [
+  {
+    id: 0,
+    title: "Meet Siphome",
+    description:
+      "The most comprehensive smart solution to home maintenance. All it takes is a simple and affordable sensor installation by one of our Siphome certified technicians.",
+  },
+  {
+    id: 1,
+    image: "./icon1.png",
+    title: "Automatic Threat Detection",
+    description:
+      "Siphome reacts the moment your equipment fails, alerting you to the problem.",
+  },
+  {
+    id: 2,
+    image: "./icon2.png",
+    title: "Smart Diagnostics",
+    description:
+      "See exactly what is malfunctioning and compare your maintenance options on the same screen.",
+  },
+  {
+    id: 3,
+    image: "./icon3.png",
+    title: "Disaster Prevention",
+    description:
+      "Predictive analytics lets you see what needs maintenance before it fails.",
+  },
+  {
+    id: 4,
+    image: "./icon4.png",
+    title: "24/7 Service",
+    description:
+      "Siphome monitors your critical systems 24/7 so you can enjoy peace of mind.",
+  },
+];
