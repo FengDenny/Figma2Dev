cd7493f3dcd639594fd48f7454ac20b19d826b60
FengDenny <fengdennyy@gmail.com>
2022-09-13 20:05:58 -0700

    Added Service/ Testimonial section

--- Siphome/client/src/pages/Home/Home.jsx
+++ Siphome/client/src/pages/Home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Features from "../../components/Features/Features";
 import Update from "../../components/Update/Update";
+import Services from "../../components/Services/Services";
+import Testimonials from "../../components/Testimonials/Testimonials";
 export default function Home() {
   return (
     <main>
       <Hero />
       <Features />
       <Update />
+      <Services />
+      <Testimonials />
     </main>
   );
 }
