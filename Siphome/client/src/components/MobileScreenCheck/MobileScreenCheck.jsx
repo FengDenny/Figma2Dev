21bd59d31bc6a9fdf55db1abda3a976fa7d393a9
FengDenny <fengdennyy@gmail.com>
2022-09-13 20:03:32 -0700

    Check mobile screen to change layout/ content

---
+++ Siphome/client/src/components/MobileScreenCheck/MobileScreenCheck.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+export default function MobileScreenCheck() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= 997;
+}
