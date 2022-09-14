fc8dd59c9db71c7d4cc032434650c4f3eb068054
FengDenny <fengdennyy@gmail.com>
2022-09-14 13:44:50 -0700

    Changed home ahref to / instead of #home

--- Siphome/client/src/data/navbar.js
+++ Siphome/client/src/data/navbar.js
@@ -1,5 +1,5 @@
 export const links = [
-  { id: 1, ahref: "#home", link: "Home" },
+  { id: 1, ahref: "/", link: "Home" },
   { id: 2, ahref: "#features", link: "Features" },
   { id: 3, ahref: "#services", link: "Services" },
   { id: 4, ahref: "#contact", link: "Contact" },
