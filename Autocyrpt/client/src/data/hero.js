107d3829212aa5663033af9dd099b6227eb65679
FengDenny <fengdennyy@gmail.com>
2022-09-16 18:11:02 -0700

    Added data for hero section

---
+++ Autocyrpt/client/src/data/hero.js
@@ -0,0 +1,11 @@
+export const hero = [
+  {
+    id: 0,
+    header: "Easiest way to mining your bitcoin",
+    description:
+      "Autocrypt is the place to buy and sell Bitcoin, Ethereum and various kinds of Digital Assets in Indonesia. Then, book your weekly, bi-weekly or one-off clean online with imminent confirmation.",
+    buttonTitle: "Get Started",
+    image: "hero-img.png",
+    alt: "Autocrypt App",
+  },
+];
