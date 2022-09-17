87b9cd771170f4a2ab5249daf1b5b5b460f7816e
FengDenny <fengdennyy@gmail.com>
2022-09-16 18:13:50 -0700

    Created mobile menu for mobile viewport

--- Autocyrpt/client/src/component/Navbar/mobile/MobileMenu.jsx
+++ Autocyrpt/client/src/component/Navbar/mobile/MobileMenu.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import styles from "./Mobile.module.scss";
 import { IoIosClose } from "react-icons/io";
-// import { navigations } from "../../data/navigation";
+import { navigations } from "../../../data/navigation";
+import RegisterBtn from "../NavButtons/Register";
+import LoginBtn from "../NavButtons/Login";
 
 export default function MobileMenu(props) {
-  let menu = [`${styles.sideMenu}`];
-
-  if (props.show) {
-    menu = [`${styles.sideMenu}`, `${styles.open}`];
-  }
-
   return (
-    <nav className={menu.join(" ")}>
+    <nav className={`${styles.sideMenu} ${props.show && styles.open}`}>
       <button className={styles.closeBtn} onClick={props.close}>
         <IoIosClose size={40} />
       </button>
+      <ul className={styles.navItems}>
+        {navigations.map((link) => (
+          <li key={link.id}>
+            <a href={link.ahref}>{link.name}</a>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.navButton}>
+        <div className={styles.buttonNoStyle}>
+          <LoginBtn />
+        </div>
+        <div className={styles.buttonStyle}>
+          <RegisterBtn />
+        </div>
+      </div>
     </nav>
   );
 }
