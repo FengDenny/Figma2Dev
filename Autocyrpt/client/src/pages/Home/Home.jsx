5eea65f4e74aa2d338b8c51c2d9c2c965558473b
FengDenny <fengdennyy@gmail.com>
2022-09-18 15:13:45 -0700

    Benefits/Pricing section finished

--- Autocyrpt/client/src/pages/Home/Home.jsx
+++ Autocyrpt/client/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../../component/Hero/Hero";
+import Benefits from "../../component/Benefit/Benefits";
+import Pricing from "../../component/Pricing/Pricing";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Benefits />
+      <Pricing />
     </div>
   );
 }
