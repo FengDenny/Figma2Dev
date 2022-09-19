00a9a4c1fd0cfe0ebeb7e940ac6fb0a7a2abe292
FengDenny <fengdennyy@gmail.com>
2022-09-18 20:14:50 -0700

    data updated

--- Autocyrpt/client/src/data/navigation.js
+++ Autocyrpt/client/src/data/navigation.js
@@ -2,5 +2,5 @@ export const navigations = [
   { id: 0, ahref: "#benefits", name: "Benefit" },
   { id: 1, ahref: "#pricing", name: "Pricing" },
   { id: 2, ahref: "#testimonials", name: "Testimonials" },
-  { id: 3, ahref: "#client", name: "Client" },
+  { id: 3, ahref: "#client", name: "Clients" },
 ];
