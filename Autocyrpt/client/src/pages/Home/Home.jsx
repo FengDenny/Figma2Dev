d18a9b7d9b676fc9b11166887c2a71d79e437c8a
FengDenny <fengdennyy@gmail.com>
2022-09-18 17:18:09 -0700

    Added Testimonial section to Home

--- Autocyrpt/client/src/pages/Home/Home.jsx
+++ Autocyrpt/client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../component/Hero/Hero";
 import Benefits from "../../component/Benefit/Benefits";
 import Pricing from "../../component/Pricing/Pricing";
+import Testimonials from "../../component/Testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Benefits />
       <Pricing />
+      <Testimonials />
     </div>
   );
 }
