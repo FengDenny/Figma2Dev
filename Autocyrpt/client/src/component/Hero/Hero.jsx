e1b2e28a9a4947ab45c845b4f0978a71aab2005b
FengDenny <fengdennyy@gmail.com>
2022-09-18 23:23:01 -0700

    Created Animations using our animations hook

--- Autocyrpt/client/src/component/Hero/Hero.jsx
+++ Autocyrpt/client/src/component/Hero/Hero.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Hero.module.scss";
 import global from "../../global.module.scss";
 import { hero } from "../../data/hero";
 import GetStartedButton from "./Button/GetStartedButton";
 import animate from "../Animations/animations.module.scss";
+import Animate from "../Animations/AnimateOnScroll";
 
 export default function Hero() {
+  const [visible, setVisible] = useState();
   return (
     <section className={styles.container}>
       <div className={global.container}>
         {hero.map((data) => (
           <div key={data.id} className={styles.hero}>
             <div className={styles.header}>
-              <h1 className={animate.fadeTop}>{data.header}</h1>
-              <h3 className={animate.fadeBottom}>{data.description}</h3>
-              <div className={`${styles.button} ${animate.opacity}`}>
-                <GetStartedButton title={data.buttonTitle} />
-              </div>
+              <Animate
+                setVisible={setVisible}
+                content={
+                  <h1 className={`${visible && animate.fadeTop}`}>
+                    {data.header}
+                  </h1>
+                }
+              />
+              <Animate
+                setVisible={setVisible}
+                content={
+                  <h3 className={`${visible && animate.fadeBottom}`}>
+                    {data.description}
+                  </h3>
+                }
+              />
+              <Animate
+                setVisible={setVisible}
+                content={
+                  <div
+                    className={`${styles.button} ${visible && animate.opacity}`}
+                  >
+                    <GetStartedButton title={data.buttonTitle} />
+                  </div>
+                }
+              />
             </div>
-            <div className={`${styles.heroImage} ${animate.opacity}`}>
-              <img src={require(`../../image/${data.image}`)} alt={data.alt} />
-            </div>
+            <Animate
+              setVisible={setVisible}
+              content={
+                <div
+                  className={`${styles.heroImage} ${
+                    visible && animate.opacity
+                  }`}
+                >
+                  <img
+                    src={require(`../../image/${data.image}`)}
+                    alt={data.alt}
+                  />
+                </div>
+              }
+            />
           </div>
         ))}
       </div>
