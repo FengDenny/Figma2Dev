90a1d3f2aa7b16e038146415630469b3df4289a3
FengDenny <fengdennyy@gmail.com>
2022-09-18 23:22:33 -0700

    Scroll animations using IntersectionObserver

---
+++ Autocyrpt/client/src/component/Animations/AnimateOnScroll.jsx
@@ -0,0 +1,14 @@
+import React, { useLayoutEffect, useRef } from "react";
+
+export default function AnimateOnScroll({ setVisible, content }) {
+  const ref = useRef();
+  useLayoutEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setVisible(entry.isIntersecting);
+    });
+    observer.observe(ref.current);
+  }, []);
+
+  return <div ref={ref}>{content}</div>;
+}
