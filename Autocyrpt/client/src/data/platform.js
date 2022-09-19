f3245c27602c07f23436e4d2fb11465ac36cb68a
FengDenny <fengdennyy@gmail.com>
2022-09-18 20:14:54 -0700

    Create platform.js

---
+++ Autocyrpt/client/src/data/platform.js
@@ -0,0 +1,32 @@
+export const platform = [
+  {
+    id: 0,
+    company: "Amazon",
+    logo: "amazon.png",
+  },
+  {
+    id: 1,
+    company: "Etsy",
+    logo: "Etsy.png",
+  },
+  {
+    id: 2,
+    company: "ClickBank",
+    logo: "clickbank.png",
+  },
+  {
+    id: 3,
+    company: "Logo",
+    logo: "logo.png",
+  },
+  {
+    id: 4,
+    company: "Youtube",
+    logo: "Youtube.png",
+  },
+  {
+    id: 5,
+    company: "Kickstarter",
+    logo: "Kickstarter.png",
+  },
+];
