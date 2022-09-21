e95cde0da9c5e2156ed2437be193abe05abe30ed
FengDenny <fengdennyy@gmail.com>
2022-09-20 19:30:44 -0700

    Fetched TMDB now playing get request

---
+++ TheMovieBox/client/src/component/Trending/NowPlaying.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { fetchRequest } from "../../api/FetchRquests/FetchRequest";
+import { getMoviesEndpoint } from "../../api/endpoint/movies";
+import { getImage } from "../../api/endpoint/image";
+import { key } from "../../api/TMBDKey.js";
+
+export default function NowPlaying() {
+  const nowPlaying = getMoviesEndpoint("now_playing", key, 1);
+  const [result, getResult] = useState([]);
+  useEffect(() => {
+    try {
+      fetchRequest(nowPlaying, "GET", key).then((response) => {
+        getResult(response.data.results);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  return <div>NowPlaying</div>;
+}
