231575909dd60485dd9c4eceafa815a6cea753cc
FengDenny <fengdennyy@gmail.com>
2022-09-21 09:42:23 -0700

    Created mobile nav buttons

--- TheMovieBox/client/src/component/Navbar/mobile/MobileMenu.jsx
+++ TheMovieBox/client/src/component/Navbar/mobile/MobileMenu.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./MobileMenu.module.scss";
 import Searchbar from "../Searchbar/Searchbar";
+import Login from "../NavButtons/Login";
+import Signup from "../NavButtons/Signup";
 
 export default function MobileMenu({ show, close }) {
   return (
     <div className={`${styles.mobileNavWrapper} ${show ? styles.open : null}`}>
       <Searchbar />
+      <div className={styles.mobileButton}>
+        <Login mobile />
+        <Signup mobile />
+      </div>
     </div>
   );
 }
