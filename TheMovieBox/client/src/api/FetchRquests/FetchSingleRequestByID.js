8307da2eaf8b9563018ba5ad2fb7bbb82f30258d
FengDenny <fengdennyy@gmail.com>
2022-09-22 19:19:11 -0700

    Fetch a single request by  id from TMDB API

---
+++ TheMovieBox/client/src/api/FetchRquests/FetchSingleRequestByID.js
@@ -0,0 +1,30 @@
+// T.C O(n)
+export default function FetchSingleRequestByID(response, dataID) {
+  return response.reduce(
+    (
+      acc,
+      {
+        id,
+        title,
+        release_date,
+        vote_average,
+        backdrop_path,
+        original_language,
+        vote_count,
+      }
+    ) => {
+      id.toString().includes(dataID) &&
+        acc.push({
+          id,
+          title,
+          backdrop_path,
+          release_date,
+          vote_average,
+          original_language,
+          vote_count,
+        });
+      return acc;
+    },
+    []
+  );
+}
