5639edb745fd497aa02f429c40244ddfd88e447b
FengDenny <fengdennyy@gmail.com>
2022-09-22 19:18:42 -0700

    Created Hero section

---
+++ TheMovieBox/client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "../../component/Hero/Hero";
+import NowPlaying from "../../component/Trending/NowPlaying";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
