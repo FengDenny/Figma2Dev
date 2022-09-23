6c43710d63db2d982a15bcf73f548d68e009ff11
FengDenny <fengdennyy@gmail.com>
2022-09-22 19:19:39 -0700

    added YouTube trailers URL

--- TheMovieBox/client/src/api/endpoint/movies.js
+++ TheMovieBox/client/src/api/endpoint/movies.js
@@ -1,3 +1,11 @@
 export const getMoviesEndpoint = (type, key, pages) => {
   return `https://api.themoviedb.org/3/movie/${type}?api_key=${key}&language=en-US&page=${pages}&region=US`;
 };
+
+export const youtubeWatchURL = (key) => {
+  return `https://www.youtube.com/embed/${key}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=0&playlist=${key}`;
+};
+
+export const getMovieYTTrailers = (id, key) => {
+  return `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${key}&language=en-US`;
+};
