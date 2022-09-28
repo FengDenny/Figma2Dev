53472ac1e7519d130ba2976bd7ac6b56a0b64552
FengDenny <fengdennyy@gmail.com>
2022-09-27 17:12:50 -0700

    added get  movie by genre endpoint from TMDB

--- TheMovieBox/client/src/api/endpoint/genre.js
+++ TheMovieBox/client/src/api/endpoint/genre.js
@@ -1,3 +1,7 @@
 export const getMovieGenre = (key) => {
   return `https://api.themoviedb.org/3/genre/movie/list?api_key=${key}&language=en-US`;
 };
+
+export const getMovieByGenre = (type, key, genreID) => {
+  return `https://api.themoviedb.org/3/movie/${type}?api_key=${key}&language=en-US&page=1&region=US&with_genres=${genreID}`;
+};
