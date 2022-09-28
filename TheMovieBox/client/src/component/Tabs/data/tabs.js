220d7850ae82fbeb5e512f6efa407ba1bfbbd47e
FengDenny <fengdennyy@gmail.com>
2022-09-27 17:14:32 -0700

    Tab navigation data

---
+++ TheMovieBox/client/src/component/Tabs/data/tabs.js
@@ -0,0 +1,6 @@
+export const tabs = [
+  { id: 0, title: "Trending", subtitle: "now_playing" },
+  { id: 1, title: "Top Rated", subtitle: "popular" },
+  { id: 2, title: "New Arrivals", subtitle: "upcoming" },
+  { id: 3, title: "Genre", subtitle: "genre" },
+];
