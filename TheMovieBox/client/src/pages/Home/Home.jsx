200b727cf9605784758067d5b8aac42b7d08071c
FengDenny <fengdennyy@gmail.com>
2022-09-30 00:25:49 -0700

    Added top scroll

--- TheMovieBox/client/src/pages/Home/Home.jsx
+++ TheMovieBox/client/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "../../component/Hero/Hero";
 import Tab from "../../component/Tabs/Tabs";
-
+import ScrollTop from "../../component/ScrollTop/ScrollTopArrow";
 export default function Home() {
   return (
     <div>
       <Hero />
       <Tab />
+      <ScrollTop />
     </div>
   );
 }
