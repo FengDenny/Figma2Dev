61d8128e7ae6f78833fcb1cd06e79e987d046e21
FengDenny <fengdennyy@gmail.com>
2022-09-29 22:34:17 -0700

    Update useInfiniteScroll.jsx

--- TheMovieBox/client/src/hooks/useInfiniteScroll.jsx
+++ TheMovieBox/client/src/hooks/useInfiniteScroll.jsx
@@ -4,23 +4,25 @@ export const useInfiniteScroll = (callback) => {
   const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", isScrolling);
-    return () => window.removeEventListener("scrolling", isScrolling);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   useEffect(() => {
     if (!isFetching) return;
     callback();
   }, [isFetching]);
 
-  function isScrolling() {
+  function handleScroll() {
     if (
       window.innerHeight + document.documentElement.scrollTop !==
         document.documentElement.offsetHeight ||
       isFetching
-    ) {
+    )
       return;
-    }
     setIsFetching(true);
   }
+
   return [isFetching, setIsFetching];
 };
