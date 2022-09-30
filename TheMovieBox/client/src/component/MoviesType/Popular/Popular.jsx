369ee86bb83c58310e1c7ff494e16bac7b7116b6
FengDenny <fengdennyy@gmail.com>
2022-09-29 18:56:48 -0700

    MoviesType components integrated with custom hooks to populate data

--- TheMovieBox/client/src/component/MoviesType/Popular/Popular.jsx
+++ TheMovieBox/client/src/component/MoviesType/Popular/Popular.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import global from "../../../global.module.scss";
+import styles from "../movieTypes.module.scss";
+import { getImage } from "../../../api/endpoint/image";
+import { useGetMovies } from "../../../hooks/useGetMovies";
+import { useGetGenre } from "../../../hooks/useGetGenre";
 
 export default function Popular() {
-  return <div>Popular</div>;
+  const popular = useGetMovies("popular", "GET");
+  const genre = useGetGenre();
+
+  return (
+    <ol className={global.gridWrapper}>
+      {Array.isArray(popular) &&
+        popular.map((data) => (
+          <li className={styles.container} key={data.id}>
+            <div className={styles.image}>
+              <img src={getImage(data.backdrop_path)} alt={data.title} />
+            </div>
+            <div className={styles.header}>
+              <h2>{data.title}</h2>
+              <h5 className={styles.date}>
+                {data.release_date.split("-").slice(0, 1)}
+              </h5>
+              <div className={`${global.dFlexRow} ${global.spaceBetween}`}>
+                <div className={styles.genres}>
+                  {data.genre_ids.map((id) => (
+                    <h2 key={genre[id]}>{genre[id]}</h2>
+                  ))}
+                </div>
+                <div className={styles.rating}>{data.vote_average}</div>
+              </div>
+            </div>
+          </li>
+        ))}
+    </ol>
+  );
 }
--- TheMovieBox/client/src/component/MoviesType/Trending/NowPlaying.jsx
+++ TheMovieBox/client/src/component/MoviesType/Trending/NowPlaying.jsx
@@ -1,20 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { fetchRequest } from "../../../api/FetchRquests/FetchRequest";
-import { getMoviesEndpoint } from "../../../api/endpoint/movies";
+import global from "../../../global.module.scss";
+import styles from "../movieTypes.module.scss";
 import { getImage } from "../../../api/endpoint/image";
-import { key } from "../../../api/TMBDKey.js";
+import { useGetMovies } from "../../../hooks/useGetMovies";
+import { useGetGenre } from "../../../hooks/useGetGenre";
 
 export default function NowPlaying() {
-  const nowPlaying = getMoviesEndpoint("now_playing", key, 1);
-  const [result, getResult] = useState([]);
-  useEffect(() => {
-    try {
-      fetchRequest(nowPlaying, "GET", key).then((response) => {
-        getResult(response.data.results);
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-  return <div>NowPlaying</div>;
+  const nowPlaying = useGetMovies("now_playing", "GET");
+  const genre = useGetGenre();
+
+  return (
+    <ol className={global.gridWrapper}>
+      {Array.isArray(nowPlaying) &&
+        nowPlaying.map((data) => (
+          <li className={styles.container} key={data.id}>
+            <div className={styles.image}>
+              <img src={getImage(data.backdrop_path)} alt={data.title} />
+            </div>
+            <div className={styles.header}>
+              <h2>{data.title}</h2>
+              <h5 className={styles.date}>
+                {data.release_date.split("-").slice(0, 1)}
+              </h5>
+              <div className={`${global.dFlexRow} ${global.spaceBetween}`}>
+                <div className={styles.genres}>
+                  {data.genre_ids.map((id) => (
+                    <h2 key={genre[id]}>{genre[id]}</h2>
+                  ))}
+                </div>
+                <div className={styles.rating}>{data.vote_average}</div>
+              </div>
+            </div>
+          </li>
+        ))}
+    </ol>
+  );
 }
