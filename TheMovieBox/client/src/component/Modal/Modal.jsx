585650305b11a42c3cfde49eabf7838dc4c8b9e1
FengDenny <fengdennyy@gmail.com>
2022-10-02 16:54:20 -0700

    refactored button/h3 tag from Hero.jsx to Modal.jsx

--- TheMovieBox/client/src/component/Modal/Modal.jsx
+++ TheMovieBox/client/src/component/Modal/Modal.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Modal.module.scss";
+import { ImCross } from "react-icons/im";
 export default function Modal({
   className,
   show,
   children,
   activeStyle,
   hiddenStyle,
+  title,
+  handleClose,
+  headerClass,
 }) {
   return (
     <div
@@ -13,6 +17,10 @@ export default function Modal({
         show ? activeStyle : hiddenStyle
       } `}
     >
+      <h3 className={headerClass}>{title}</h3>
+      <button onClick={handleClose}>
+        <ImCross />
+      </button>
       {children}
     </div>
   );
