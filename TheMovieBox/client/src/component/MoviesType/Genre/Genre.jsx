8841ef8a34d3b53f641adcb3457c45eb5095ce48
FengDenny <fengdennyy@gmail.com>
2022-10-04 19:25:12 -0700

    created genre selection to select genre and display genre result

--- TheMovieBox/client/src/component/MoviesType/Genre/Genre.jsx
+++ TheMovieBox/client/src/component/MoviesType/Genre/Genre.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import styles from "./Genre.module.scss";
+import { key } from "../../../api/TMBDKey.js";
+import { fetchRequest } from "../../../api/FetchRquests/FetchRequest";
+import { getMovieGenre } from "../../../api/endpoint/genre";
+import global from "../../../global.module.scss";
+import TopMovieGenre from "../Genre/TopMovieGenre";
 export default function Genre() {
-  return <div>Genre</div>;
+  const [genres, setGenres] = useState([]);
+  const genre = getMovieGenre(key);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    try {
+      fetchRequest(genre, "GET", key).then((response) => {
+        setGenres(response.data.genres);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <div className={global.container}>
+      <form className={styles.form}>
+        <select onChange={(e) => setValue(e.target.value)} size='3'>
+          {genres.map((data) => {
+            return (
+              data.id !== 99 && (
+                <option value={data.id} key={data.id}>
+                  {data.name}
+                </option>
+              )
+            );
+          })}
+        </select>
+      </form>
+      <div>
+        <TopMovieGenre value={value} />
+      </div>
+    </div>
+  );
 }
