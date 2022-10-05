13f783f6e0b448c6843cc527182ec646b6677dbf
FengDenny <fengdennyy@gmail.com>
2022-10-04 19:24:16 -0700

    Created MovieInfo component

---
+++ TheMovieBox/client/src/component/MoviesType/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import VerticalLine from "../../VerticalLine/VerticalLine";
+import { useGetGenre } from "../../../hooks/useGetGenre";
+import styles from "./MovieInfo.module.scss";
+
+export default function MovieInfo({
+  data: { original_language, release_date, vote_average, overview, genre_ids },
+}) {
+  const genre = useGetGenre();
+  return (
+    <>
+      <div className={styles.modalSmallHeader}>
+        <div>
+          Original language:
+          <span>{original_language}</span>
+        </div>
+        <VerticalLine />
+        <div>
+          Release date:
+          <span>{release_date}</span>
+        </div>
+        <VerticalLine />
+        <div>
+          Rating:
+          <span>{vote_average}</span>
+        </div>
+      </div>
+      <div className={styles.description}>
+        Overview: <span>{overview}</span>
+      </div>
+      <div className={styles.genres}>
+        {genre_ids.map((id) => (
+          <span key={genre[id]}>{genre[id]}</span>
+        ))}
+      </div>
+    </>
+  );
+}
