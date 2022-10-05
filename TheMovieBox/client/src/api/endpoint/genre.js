3a1bd118724910cfec30c471643a5b202c33c196
FengDenny <fengdennyy@gmail.com>
2022-10-04 19:18:49 -0700

    Added getTopMovieByGenre endpoint

--- TheMovieBox/client/src/api/endpoint/genre.js
+++ TheMovieBox/client/src/api/endpoint/genre.js
@@ -5,3 +5,7 @@ export const getMovieGenre = (key) => {
 export const getMovieByGenre = (type, key, genreID) => {
   return `https://api.themoviedb.org/3/movie/${type}?api_key=${key}&language=en-US&page=1&region=US&with_genres=${genreID}`;
 };
+
+export const getTopMovieByGenre = (key, genreID, page, release_date) => {
+  return `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${page}&region=US&with_genres=${genreID}&primary_release_date.gte=${release_date}`;
+};
