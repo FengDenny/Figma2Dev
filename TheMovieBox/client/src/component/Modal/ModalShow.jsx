77700ccd10f586a0dceb15b9cfd1d65743de14bf
FengDenny <fengdennyy@gmail.com>
2022-11-29 17:48:44 -0800

    Created Authentication Modal for Log in / Sign up

--- TheMovieBox/client/src/component/Modal/ModalShow.jsx
+++ TheMovieBox/client/src/component/Modal/ModalShow.jsx
@@ -2,6 +2,8 @@ import Modal from "./Modal";
 import AnimationStyles from "./Animations.module.scss";
 import MovieInfo from "../MoviesType/MovieInfo/MovieInfo";
 import MovieInfoStyles from "../MoviesType/MovieInfo/MovieInfo.module.scss";
+import LoginAuth from "../Navbar/NavButtons/Auth/Login/LoginAuthModal";
+import RegisterAuth from "../Navbar/NavButtons/Auth/Register/RegisterAuthModal";
 
 export const ModalShow = ({ showModal, closeModal, id, data }) => {
   return (
@@ -42,6 +44,38 @@ export const ModalHeroShow = ({ showModal, closeModal, data }) => {
             title={data.title}
           >
             <MovieInfo data={data} />
+          </Modal>
+        </div>
+      )}
+    </>
+  );
+};
+
+export const ModalAuthShow = ({
+  showModal,
+  closeModal,
+  title,
+  active,
+  setActive,
+}) => {
+  return (
+    <>
+      {showModal && (
+        <div className={MovieInfoStyles.darkBG}>
+          <Modal
+            show={showModal}
+            className={MovieInfoStyles.heroModal}
+            activeStyle={AnimationStyles.active}
+            hiddenStyle={AnimationStyles.hidden}
+            handleClose={closeModal}
+            headerClass={MovieInfoStyles.modalHeader}
+            title={title}
+          >
+            {active === "login" ? (
+              <LoginAuth setActive={setActive} />
+            ) : (
+              <RegisterAuth setActive={setActive} />
+            )}
           </Modal>
         </div>
       )}
