87c0263b6e1308a78bba7dd5ea2a0216c266dda5
FengDenny <fengdennyy@gmail.com>
2022-11-29 17:50:07 -0800

    Added button to open Log in modal

--- TheMovieBox/client/src/component/Navbar/NavButtons/Login.jsx
+++ TheMovieBox/client/src/component/Navbar/NavButtons/Login.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./buttons.module.scss";
+import { ModalAuthShow } from "../../Modal/ModalShow";
+import {
+  openHeroModal,
+  closeModal,
+} from "../../Modal/ModalHelper/ModalHelpers";
 
 export default function Login({ mobile }) {
+  const [showModal, setShowModal] = useState(false);
+  const [active, setActive] = useState("login");
   return (
-    <button className={mobile ? styles.loginBtnMobile : styles.loginBtnDesktop}>
-      Login
-    </button>
+    <>
+      <button
+        className={mobile ? styles.loginBtnMobile : styles.loginBtnDesktop}
+        onClick={() => openHeroModal(setShowModal, showModal)}
+      >
+        Log in
+      </button>
+
+      {
+        <ModalAuthShow
+          active={active}
+          setActive={setActive}
+          title={
+            active === "login" ? "Log in to your account" : "Sign up For Free"
+          }
+          showModal={showModal}
+          setShowModal={setShowModal}
+          closeModal={() => closeModal(setShowModal)}
+        />
+      }
+    </>
   );
 }
