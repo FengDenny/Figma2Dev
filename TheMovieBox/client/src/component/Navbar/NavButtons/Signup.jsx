a300e009a192731eaa4c4b60acf9dc2110093fee
FengDenny <fengdennyy@gmail.com>
2022-11-29 17:50:17 -0800

    Added button to open Sign up modal

--- TheMovieBox/client/src/component/Navbar/NavButtons/Signup.jsx
+++ TheMovieBox/client/src/component/Navbar/NavButtons/Signup.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./buttons.module.scss";
 
+import { ModalAuthShow } from "../../Modal/ModalShow";
+import {
+  openHeroModal,
+  closeModal,
+} from "../../Modal/ModalHelper/ModalHelpers";
+
 export default function Signup({ mobile }) {
+  const [showModal, setShowModal] = useState(false);
+  const [active, setActive] = useState("register");
+
   return (
-    <button
-      className={mobile ? styles.signupBtnMobile : styles.signupBtnDesktop}
-    >
-      Signup
-    </button>
+    <>
+      <button
+        className={mobile ? styles.signupBtnMobile : styles.signupBtnDesktop}
+        onClick={() => openHeroModal(setShowModal, showModal)}
+      >
+        Sign up
+      </button>
+
+      {
+        <ModalAuthShow
+          active={active}
+          setActive={setActive}
+          title={
+            active === "login" ? "Log in to your account" : "Sign up For Free"
+          }
+          showModal={showModal}
+          closeModal={() => closeModal(setShowModal)}
+        />
+      }
+    </>
   );
 }
