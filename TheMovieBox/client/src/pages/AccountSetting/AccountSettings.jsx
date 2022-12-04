cfd593333f0b2cff387e288e62dca01e0a5140e4
FengDenny <fengdennyy@gmail.com>
2022-12-03 19:45:44 -0800

    Created account setting pages/ CRU operation

--- TheMovieBox/client/src/pages/AccountSetting/AccountSettings.jsx
+++ TheMovieBox/client/src/pages/AccountSetting/AccountSettings.jsx
@@ -1,29 +1,114 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
 import global from "../../global.module.scss";
 import styles from "./AccountSetting.module.scss";
+import Tabs from "../AccountSetting/SettingTabs/Tabs";
+
+//  firebase
+import {
+  getAuth,
+  updateProfile,
+  updateEmail,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { database } from "../../firebase/firebaseConfig";
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { userAction } from "../../redux/slice/auth/userData-slice";
 
 export default function AccountSettings() {
   const { userData } = useSelector((state) => ({ ...state }));
-  const { userInfo, metadata } = userData;
+  const { userInfo, metadata, usersCollectionID } = userData;
   const { email, displayName } = userInfo;
   const { creationTime, lastSignInTime } = metadata;
+  const { userID } = usersCollectionID;
+  const [newData, setNewData] = useState({
+    newEmail: "",
+    newFullName: "",
+    newPassword: "",
+  });
+  const auth = getAuth();
+  const dispatch = useDispatch();
+  const collectionRef = collection(database, "users");
+  useEffect(() => {
+    getUserID();
+  }, []);
 
-  console.log(userData);
+  const getUserID = async () => {
+    const data = await getDocs(collectionRef);
+    data.docs.map((item) => {
+      return dispatch(
+        userAction.addUserCollectionID({
+          userID: item.id,
+        })
+      );
+    });
+  };
+  const newEmail = newData.newEmail;
+
+  const handleIncomingUpdatedData = (event) => {
+    event.preventDefault();
+    let inputs = { [event.target.name]: event.target.value };
+    setNewData({ ...newData, ...inputs });
+  };
+
+  const updateAccountData = async () => {
+    let dataToUpdate = doc(database, "users", userID);
+
+    await updateProfile(auth.currentUser, {
+      displayName:
+        newData.newFullName !== "" ? newData.newFullName : displayName,
+    })
+      .then(() => {
+        dispatch(
+          userAction.addUserAccountInfo({
+            displayName:
+              newData.newFullName !== "" ? newData.newFullName : displayName,
+          })
+        );
+      })
+      .catch((err) => console.log(err.message));
+
+    await updateEmail(auth.currentUser, newEmail !== "" ? newEmail : email)
+      .then(() => {
+        dispatch(
+          userAction.addUserAccountInfo({
+            email: newData.newEmail !== "" ? newData.newEmail : email,
+          })
+        );
+      })
+      .catch((err) => console.log(err.message));
+
+    await updateDoc(dataToUpdate, {
+      email: newData.newEmail !== "" ? newData.newEmail : email,
+      displayName:
+        newData.newFullName !== "" ? newData.newFullName : displayName,
+    })
+      .then(() => console.log("Data has been updated! Please log in!"))
+      .catch((err) => console.log(err.message));
+  };
+
   return (
     <section className={global.container}>
       <div className={styles.settings}>
-        <h2 className={styles.header}>Account Settings</h2>
-        <div className={styles.accountInfo}>
-          <div className={styles.accountUserInfo}>
-            <h4>Email: {email}</h4>
-            <h4>Full name: {displayName}</h4>
-          </div>
-          <div className={styles.accountMetaInfo}>
-            <h4> CreatedAt: {creationTime}</h4>
-            <h4> Last Logged In: {lastSignInTime}</h4>
-          </div>
-        </div>
+        <Tabs
+          styles={styles}
+          email={email}
+          displayName={displayName}
+          creationTime={creationTime.substring(0, 16)}
+          lastSignInTime={lastSignInTime.substring(0, 16)}
+          handleIncomingUpdatedData={handleIncomingUpdatedData}
+          updateAccountData={updateAccountData}
+          newData={newData}
+          userID={userID}
+        />
       </div>
     </section>
   );
