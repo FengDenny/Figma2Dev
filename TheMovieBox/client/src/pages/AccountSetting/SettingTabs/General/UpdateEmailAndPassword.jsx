47296aa3316cb97ec6104f941fb0cfc15524a9f9
FengDenny <fengdennyy@gmail.com>
2022-12-03 19:45:02 -0800

    Create UpdateEmailAndPassword.jsx

---
+++ TheMovieBox/client/src/pages/AccountSetting/SettingTabs/General/UpdateEmailAndPassword.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import AccountCreationAndLoggedInfo from "../../SettingTabs/MetaData/AccountCreationAndLoggedInfo";
+
+export default function UpdateEmailAndPassword({
+  styles,
+  email,
+  displayName,
+  creationTime,
+  lastSignInTime,
+  handleIncomingUpdatedData,
+  updateAccountData,
+  newData,
+}) {
+  return (
+    <>
+      <h2 className={styles.header}>Account Settings</h2>
+      <div className={styles.accountInfo}>
+        <div className={styles.accountUserInfo}>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='newEmail'
+            placeholder={email}
+            value={newData.newEmail}
+            onChange={(event) => handleIncomingUpdatedData(event)}
+          />
+        </div>
+        <div className={styles.accountUserInfo}>
+          <label htmlFor='name'>Full Name</label>
+          <input
+            type='text'
+            name='newFullName'
+            placeholder={displayName}
+            value={newData.newFullName}
+            onChange={(event) => handleIncomingUpdatedData(event)}
+          />
+        </div>
+
+        <AccountCreationAndLoggedInfo
+          styles={styles}
+          creationTime={creationTime}
+          lastSignInTime={lastSignInTime}
+        />
+
+        <button className={styles.btnSave} onClick={updateAccountData}>
+          Save changes
+        </button>
+      </div>
+    </>
+  );
+}
