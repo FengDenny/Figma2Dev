34e1eaea20e9a2c1ad25650891232319553c31b9
FengDenny <fengdennyy@gmail.com>
2022-12-03 19:46:25 -0800

    added authenticated tab style

--- TheMovieBox/client/src/component/Tabs/TabsContent.jsx
+++ TheMovieBox/client/src/component/Tabs/TabsContent.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./Tab.module.scss";
+import tabStyles from "../../pages/AccountSetting/SettingTabs/Tabs.module.scss";
 
-export default function TabsContent({ activeTab, tabID, children }) {
+export default function TabsContent({ auth, activeTab, tabID, children }) {
   return (
     <div
-      className={`${styles.tabContent} ${
-        activeTab === tabID ? styles.active : styles.notActive
-      }`}
+      className={`${auth ? tabStyles.tabContent : styles.tabContent} ${
+        activeTab === tabID && auth ? tabStyles.active : tabStyles.notActive
+      }
+
+      ${activeTab === tabID ? styles.active : styles.notActive}
+      `}
     >
       {children}
     </div>
