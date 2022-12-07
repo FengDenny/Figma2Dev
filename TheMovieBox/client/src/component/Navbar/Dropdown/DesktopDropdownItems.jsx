404766b479cd964d48caaf808a0b54e20c6004a5
FengDenny <fengdennyy@gmail.com>
2022-12-06 18:07:01 -0800

    Created styling for dropdown

---
+++ TheMovieBox/client/src/component/Navbar/Dropdown/DesktopDropdownItems.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function DesktopDropdownItems({ styles, uid, userLoggedOut }) {
+  return (
+    <div>
+      <ul className={styles.navUL}>
+        <li>
+          <Link to={`/trailers/my-list`} className={styles.navLink}>
+            My List
+          </Link>
+        </li>
+        <li>
+          <Link to={`/account-setting/${uid}`} className={styles.navLink}>
+            Settings
+          </Link>
+        </li>
+      </ul>
+      <button className={styles.logoutBtn} onClick={userLoggedOut}>
+        Logout
+      </button>
+    </div>
+  );
+}
