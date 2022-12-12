c861005c261df70660d9e0e52faa67e61d831553
FengDenny <fengdennyy@gmail.com>
2022-12-11 17:00:42 -0800

    Added selector data for myList page

--- TheMovieBox/client/src/pages/MyList/MyList.jsx
+++ TheMovieBox/client/src/pages/MyList/MyList.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import global from "../../global.module.scss";
 import styles from "./myList.module.scss";
+import MyListData from "./MyListData";
+import { useSelector } from "react-redux";
 
 export default function MyList() {
+  const { listData } = useSelector((state) => ({ ...state }));
+
   return (
     <section className={global.container}>
       <div className={styles.myList}>
+        <h3>My List</h3>
         <div className={styles.listCard}>
-          <h3>My List</h3>
+          <div className={global.gridWrapper}>
+            {listData.myListData.map((item) => {
+              const {
+                movieID,
+                title,
+                release_date,
+                backdrop_path,
+                genre_ids,
+                vote_average,
+              } = item;
+              return (
+                <MyListData
+                  key={movieID}
+                  movieID={movieID}
+                  title={title}
+                  release_date={release_date}
+                  backdrop_path={backdrop_path}
+                  genre_ids={genre_ids}
+                  vote_average={vote_average}
+                  item={item}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
