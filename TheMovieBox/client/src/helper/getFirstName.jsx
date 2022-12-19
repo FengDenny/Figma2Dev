59f9c0acff010d37214fb499cb6860f8d8024586
FengDenny <fengdennyy@gmail.com>
2022-12-18 17:51:19 -0800

    Delete getFirstName.jsx

--- TheMovieBox/client/src/helper/getFirstName.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-export const GetFirstName = () => {
-  const { userData } = useSelector((state) => ({ ...state }));
-  const { displayName } = userData.userInfo;
-  const firstName = displayName?.split(" ")[0];
-  return firstName;
-};
