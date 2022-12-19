946d1d362e2f054dfe29a1ba8a0310a72f0e9cc6
FengDenny <fengdennyy@gmail.com>
2022-12-18 18:13:52 -0800

    Created helper to retrieve first name in firebase

---
+++ TheMovieBox/client/src/component/Navbar/helper/GetFirstName.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { database } from "../../../firebase/firebaseConfig";
+export default function GetFirstName() {
+  const [userID, setUserID] = useState();
+  const [firstName, setFirstName] = useState();
+  const isMounted = useRef(true);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const filteredRef = query(
+      collection(database, "users"),
+      where("uid", "==", `${userID}`)
+    );
+    const getUserDoc = () =>
+      onSnapshot(filteredRef, (results) => {
+        results.docs.map((item) => setFirstName(item.data().displayName));
+      });
+
+    getUserDoc();
+  }, [userID, firstName]);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUserID(user.uid);
+        }
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  console.log(firstName);
+
+  return firstName?.split(" ")[0];
+}
