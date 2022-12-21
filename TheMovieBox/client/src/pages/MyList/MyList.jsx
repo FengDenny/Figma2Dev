e21317762db719c898a70d946c998ff88e7c92cd
FengDenny <fengdennyy@gmail.com>
2022-12-20 19:21:53 -0800

    Removed showing redux data and created persisting data from firestore with real time data

--- TheMovieBox/client/src/pages/MyList/MyList.jsx
+++ TheMovieBox/client/src/pages/MyList/MyList.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import global from "../../global.module.scss";
 import styles from "./myList.module.scss";
 import MyListData from "./MyListData";
-import { useSelector } from "react-redux";
+import { collection, doc, query, where, onSnapshot } from "firebase/firestore";
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { database } from "../../firebase/firebaseConfig";
 
 export default function MyList() {
-  const { listData } = useSelector((state) => ({ ...state }));
+  const [userID, setUserID] = useState();
+  const [docID, setDocID] = useState();
+  const [listDoc, setListDoc] = useState([]);
+
+  const isMounted = useRef(true);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const getListDoc = () => {
+      const filteredListRef = doc(database, "lists", `${docID}`);
+      onSnapshot(filteredListRef, (results) => {
+        setListDoc({ id: results.id, ...results.data() });
+      });
+    };
+    getListDoc();
+  }, [docID]);
+
+  useEffect(() => {
+    const getUserDoc = () => {
+      const filteredRef = query(
+        collection(database, "users"),
+        where("uid", "==", `${userID}`)
+      );
+      onSnapshot(filteredRef, (results) => {
+        results.docs.map((item) => setDocID(item.id));
+      });
+    };
+    getUserDoc();
+  }, [userID, docID]);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUserID(user.uid);
+        }
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return (
     <section className={global.container}>
@@ -13,28 +57,32 @@ export default function MyList() {
         <h3>My List</h3>
         <div className={styles.listCard}>
           <div className={global.gridWrapper}>
-            {listData.myListData.map((item) => {
-              const {
-                movieID,
-                title,
-                release_date,
-                backdrop_path,
-                genre_ids,
-                vote_average,
-              } = item;
-              return (
-                <MyListData
-                  key={movieID}
-                  movieID={movieID}
-                  title={title}
-                  release_date={release_date}
-                  backdrop_path={backdrop_path}
-                  genre_ids={genre_ids}
-                  vote_average={vote_average}
-                  item={item}
-                />
-              );
-            })}
+            {listDoc.movies === undefined ? (
+              <h2>Add a movie to your list.</h2>
+            ) : (
+              listDoc.movies.map((data) => {
+                const {
+                  id,
+                  title,
+                  release_date,
+                  backdrop_path,
+                  genre_ids,
+                  vote_average,
+                } = data;
+                return (
+                  <MyListData
+                    key={id}
+                    movieID={id}
+                    title={title}
+                    release_date={release_date}
+                    backdrop_path={backdrop_path}
+                    genre_ids={genre_ids}
+                    vote_average={vote_average}
+                    item={data}
+                  />
+                );
+              })
+            )}
           </div>
         </div>
       </div>
