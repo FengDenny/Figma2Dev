851381852694f0e0b3b8ed35ea7d30e64232a992
FengDenny <fengdennyy@gmail.com>
2022-12-27 14:25:35 -0800

    Added YouTube url to embed onto our hero section

--- TheMovieBox/client/src/api/endpoint/movies.js
+++ TheMovieBox/client/src/api/endpoint/movies.js
@@ -2,8 +2,12 @@ export const getMoviesEndpoint = (type, key, pages) => {
   return `https://api.themoviedb.org/3/movie/${type}?api_key=${key}&language=en-US&page=${pages}&region=US`;
 };
 
-export const youtubeWatchURL = (key) => {
+export const youTubeWatchURL = (key) => {
   return `https://www.youtube.com/embed/${key}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=0&playlist=${key}`;
+};
+
+export const embedYouTubeURL = (key) => {
+  return `https://www.youtube.com/embed/${key}?wmode=transparent&rel=0`;
 };
 
 export const getMovieYTTrailers = (id, key) => {
