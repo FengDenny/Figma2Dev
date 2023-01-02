91409dc2b717339ad7bf13b54f46c3226ac7b37d
FengDenny <fengdennyy@gmail.com>
2023-01-01 17:06:32 -0800

    Dispatch search result to redux dispatcher

--- TheMovieBox/client/src/component/Navbar/Searchbar/Searchbar.jsx
+++ TheMovieBox/client/src/component/Navbar/Searchbar/Searchbar.jsx
@@ -1,7 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import styles from "./Searchbar.module.scss";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { searchAction } from "../../../redux/slice/search/search-slice";
+
 export default function Searchbar() {
+  const navigate = useNavigate();
+
+  const [searchTitle, setSearchTitle] = useState();
+  const dispatch = useDispatch();
+
+  const handleIncomingDispatch = (event) => {
+    event.preventDefault();
+    dispatch(searchAction.searchTitle({ title: searchTitle }));
+  };
+
   return (
     <div className={styles.content}>
       <form className={styles.searchContainer}>
@@ -11,8 +25,17 @@ export default function Searchbar() {
           type='text'
           name='search'
           placeholder='Search '
+          value={searchTitle}
+          onChange={(e) => setSearchTitle(e.target.value)}
         />
-        <button className={styles.searchBtn} aria-label='submit search'>
+        <button
+          className={styles.searchBtn}
+          aria-label='submit search'
+          onClick={(e) => {
+            handleIncomingDispatch(e);
+            navigate("/search");
+          }}
+        >
           <FaSearch className={styles.searchIcon} size={20} />
         </button>
       </form>
