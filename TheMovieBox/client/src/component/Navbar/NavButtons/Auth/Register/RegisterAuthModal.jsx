cb0a787e61fe13dd4e9b6eaffeb3bdd055a3a6ff
FengDenny <fengdennyy@gmail.com>
2023-01-03 16:56:43 -0800

    Refactored password checker to its own file called DisplayError.jsx

--- TheMovieBox/client/src/component/Navbar/NavButtons/Auth/Register/RegisterAuthModal.jsx
+++ TheMovieBox/client/src/component/Navbar/NavButtons/Auth/Register/RegisterAuthModal.jsx
@@ -3,6 +3,8 @@ import authStyles from "../auths.module.scss";
 import { usePasswordChecker } from "../../helper/usePasswordChecker";
 import passwordStyles from "../../helper/passwordChecker.module.scss";
 
+import { DisplayError } from "../../helper/DisplayError";
+
 export default function RegisterAuthModal({
   setActive,
   handleIncomingData,
@@ -13,10 +15,6 @@ export default function RegisterAuthModal({
   togglePasswordVisiblity,
   passwordShown,
 }) {
-  const { error, IoIosCheckmarkCircleOutline } = usePasswordChecker(
-    data.password
-  );
-
   return (
     <form className={authStyles.authForm}>
       <div className={authStyles.formInput}>
@@ -56,64 +54,7 @@ export default function RegisterAuthModal({
           {passwordShown ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
         </i>
       </div>
-
-      {error &&
-        error.map((error, i) => (
-          <div
-            key={i}
-            className={`${passwordStyles.error} ${authStyles.formInput}`}
-          >
-            <div>
-              <span>
-                {data.password.length < 8 ? (
-                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
-                ) : (
-                  <IoIosCheckmarkCircleOutline
-                    className={passwordStyles.green}
-                  />
-                )}
-                {error.passwordLength}
-              </span>
-            </div>
-            <div>
-              <span>
-                {!data.password.match(/[A-Z]/) ? (
-                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
-                ) : (
-                  <IoIosCheckmarkCircleOutline
-                    className={passwordStyles.green}
-                  />
-                )}
-                {error.uppercased}
-              </span>
-            </div>
-            <div>
-              <span>
-                {!data.password.match(/[a-z]/) ? (
-                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
-                ) : (
-                  <IoIosCheckmarkCircleOutline
-                    className={passwordStyles.green}
-                  />
-                )}
-                {error.lowercased}
-              </span>
-            </div>
-            <div>
-              <span>
-                {!data.password.match(/[\d`~!@#$%\^&*()+=|;:'",.<>\/?\\\-]/) ? (
-                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
-                ) : (
-                  <IoIosCheckmarkCircleOutline
-                    className={passwordStyles.green}
-                  />
-                )}
-                {error.numOrSpecialCased}
-              </span>
-            </div>
-          </div>
-        ))}
-
+      {DisplayError(data)}
       <div className={authStyles.formInput}>
         <button
           type='button'
