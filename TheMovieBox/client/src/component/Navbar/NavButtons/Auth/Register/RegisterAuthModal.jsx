802423515de14be786c7f399b8503729cdabf318
FengDenny <fengdennyy@gmail.com>
2023-01-03 16:45:40 -0800

    Added a password checker before submission

--- TheMovieBox/client/src/component/Navbar/NavButtons/Auth/Register/RegisterAuthModal.jsx
+++ TheMovieBox/client/src/component/Navbar/NavButtons/Auth/Register/RegisterAuthModal.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import authStyles from "../auths.module.scss";
+import { usePasswordChecker } from "../../helper/usePasswordChecker";
+import passwordStyles from "../../helper/passwordChecker.module.scss";
 
 export default function RegisterAuthModal({
   setActive,
@@ -11,6 +13,10 @@ export default function RegisterAuthModal({
   togglePasswordVisiblity,
   passwordShown,
 }) {
+  const { error, IoIosCheckmarkCircleOutline } = usePasswordChecker(
+    data.password
+  );
+
   return (
     <form className={authStyles.authForm}>
       <div className={authStyles.formInput}>
@@ -51,11 +57,71 @@ export default function RegisterAuthModal({
         </i>
       </div>
 
+      {error &&
+        error.map((error, i) => (
+          <div
+            key={i}
+            className={`${passwordStyles.error} ${authStyles.formInput}`}
+          >
+            <div>
+              <span>
+                {data.password.length < 8 ? (
+                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
+                ) : (
+                  <IoIosCheckmarkCircleOutline
+                    className={passwordStyles.green}
+                  />
+                )}
+                {error.passwordLength}
+              </span>
+            </div>
+            <div>
+              <span>
+                {!data.password.match(/[A-Z]/) ? (
+                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
+                ) : (
+                  <IoIosCheckmarkCircleOutline
+                    className={passwordStyles.green}
+                  />
+                )}
+                {error.uppercased}
+              </span>
+            </div>
+            <div>
+              <span>
+                {!data.password.match(/[a-z]/) ? (
+                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
+                ) : (
+                  <IoIosCheckmarkCircleOutline
+                    className={passwordStyles.green}
+                  />
+                )}
+                {error.lowercased}
+              </span>
+            </div>
+            <div>
+              <span>
+                {!data.password.match(/[\d`~!@#$%\^&*()+=|;:'",.<>\/?\\\-]/) ? (
+                  <IoIosCheckmarkCircleOutline className={passwordStyles.red} />
+                ) : (
+                  <IoIosCheckmarkCircleOutline
+                    className={passwordStyles.green}
+                  />
+                )}
+                {error.numOrSpecialCased}
+              </span>
+            </div>
+          </div>
+        ))}
+
       <div className={authStyles.formInput}>
         <button
           type='button'
           className={authStyles.formSubmit}
           onClick={accountData}
+          disabled={
+            !data.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)
+          }
         >
           Sign up
         </button>
