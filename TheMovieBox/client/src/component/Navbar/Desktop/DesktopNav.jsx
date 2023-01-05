9372ffb037ab58e875120617202be853975a34a7
FengDenny <fengdennyy@gmail.com>
2023-01-05 11:13:57 -0800

    added styling for mobile link

--- TheMovieBox/client/src/component/Navbar/Desktop/DesktopNav.jsx
+++ TheMovieBox/client/src/component/Navbar/Desktop/DesktopNav.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function DesktopNav({ styles }) {
+export default function DesktopNav({ styles, mobile }) {
   return (
     <div className={styles.desktopNavItems}>
-      <ul className={styles.desktopNavUL}>
+      <ul className={`${styles.desktopNavUL} ${mobile && styles.mobileNavUL} `}>
         <li>
           <Link to='/' className={styles.desktopNavLink}>
             Home
