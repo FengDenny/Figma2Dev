586705bfb86a11b76d457c156163a08ce5a1fbb9
FengDenny <fengdennyy@gmail.com>
2023-01-08 12:45:48 -0800

    Added DisplayError to error handle our new password submissions /  added styling for our errors

--- TheMovieBox/client/src/pages/AccountSetting/SettingTabs/Security/UpdatePassword.jsx
+++ TheMovieBox/client/src/pages/AccountSetting/SettingTabs/Security/UpdatePassword.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../../AccountSetting.module.scss";
+import { DisplayError } from "../../../../component/Navbar/NavButtons/helper/DisplayError";
+import passwordStyles from "../../../../component/Navbar/NavButtons/helper/passwordChecker.module.scss";
 
 export default function UpdatePassword({
   newPasswordData,
   updateNewPassword,
   handleIncomingUpdatedData,
+  setShowHint,
+  showHint,
 }) {
   return (
     <section className={styles.security}>
@@ -24,11 +28,27 @@ export default function UpdatePassword({
           <label htmlFor='newPassword'>New Password</label>
           <input
             type='password'
-            name='newPassword'
+            name='password'
             placeholder='Create a new password'
-            value={newPasswordData.newPassword}
+            value={newPasswordData.password}
             onChange={(event) => handleIncomingUpdatedData(event)}
           />
+          <button
+            className={styles.displayHintBtn}
+            onClick={(e) => {
+              e.preventDefault();
+              setShowHint(!showHint);
+            }}
+          >
+            Show hint
+          </button>
+        </div>
+        <div
+          className={
+            showHint ? passwordStyles.showHint : passwordStyles.removeHint
+          }
+        >
+          {DisplayError(newPasswordData, "update")}
         </div>
         <div className={styles.accountUserInfo}>
           <label htmlFor='confirmPassword'>Confirm New Password</label>
@@ -40,7 +60,15 @@ export default function UpdatePassword({
             onChange={(event) => handleIncomingUpdatedData(event)}
           />
         </div>
-        <button className={styles.btnSave} onClick={updateNewPassword}>
+        <button
+          className={styles.btnSave}
+          onClick={updateNewPassword}
+          disabled={
+            !newPasswordData.password.match(
+              /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+            )
+          }
+        >
           Save changes
         </button>
       </div>
