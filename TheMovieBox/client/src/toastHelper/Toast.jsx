29283b7cc02cc4d5be78f520879bf09628363188
FengDenny <fengdennyy@gmail.com>
2023-01-08 13:28:46 -0800

    Added new toast to add own toast messages

--- TheMovieBox/client/src/toastHelper/Toast.jsx
+++ TheMovieBox/client/src/toastHelper/Toast.jsx
@@ -6,3 +6,7 @@ export const Toast = (status, data) => {
     ? toast.error(data.split("/")[1].slice(0, -2))
     : toast.success(data);
 };
+export const UpdateToast = (status, data) => {
+  console.log(status);
+  return status === "error" ? toast.error(data) : toast.success(data);
+};
