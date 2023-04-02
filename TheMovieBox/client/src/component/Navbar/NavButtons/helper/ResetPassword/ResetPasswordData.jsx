069e75a5bbbea35c28e934c688b2b0297a0e02bd
FengDenny <fengdennyy@gmail.com>
2023-04-02 13:54:59 -0700

    Changed console.log success message to show in Toast

--- TheMovieBox/client/src/component/Navbar/NavButtons/helper/ResetPassword/ResetPasswordData.jsx
+++ TheMovieBox/client/src/component/Navbar/NavButtons/helper/ResetPassword/ResetPasswordData.jsx
@@ -1,13 +1,14 @@
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { Toast } from "../../../../../toastHelper/Toast";
 
 export default async function ResetPasswordData(email) {
   const auth = getAuth();
 
   return await sendPasswordResetEmail(auth, email)
     .then(() => {
-      console.log(`Password reset link has been sent to ${email}`);
+      Toast("success", `Password reset link has been sent to ${email}`);
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err.message);
     });
 }
